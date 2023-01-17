986029331974462f4afeb1ae9ad8f6712876d1d2
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2023-01-17 13:48:16 +0100

    C

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
+
+        // $train = Train::All();
+
         return view("welcome");
     }
 }
---
+++ database/seeds/HomeTrainSeed.php
@@ -0,0 +1,16 @@
+<?php
+use app\Models\Train;
+use Illuminate\Database\Seeder;
+
+class HomeTrainSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
